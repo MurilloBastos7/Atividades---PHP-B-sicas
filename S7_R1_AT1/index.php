b75c8cee56754a7e3583a6509c904ee784931213
MurilloBastos7 <murillobastos727@gmail.com>
2026-08-11 10:30:03 -0300

    Atividade!

---
+++ S7_R1_AT1/index.php
@@ -0,0 +1,35 @@
+<?php 
+// no "$funcionarios" coloquei as informações dos colaboradores como nome, idade, etc...
+$funcionarios = [
+    ["nome" => "Murillo",
+    "cargo" => "TI Junior",
+    "setor" => "Adm",
+    "idade" => "23",],
+
+    ["nome" => "Nicollas",
+    "cargo" => "TI Superior",
+    "setor" => "Adm",
+    "idade" => "27",],
+
+    ["nome" => "Victor",
+    "cargo" => "Desenvolvedor de Software",
+    "setor" => "Adm",
+    "idade" => "32",],
+
+    ["nome" => "Miguel",
+    "cargo" => "TI Iniciante",
+    "setor" => "Adm",
+    "idade" => "19",],
+];
+// Já nesse "<hr>" serve para colocar uma linha que divide as informações no navegador
+echo "<hr>";
+
+// o comando "foreach" serve para exibir todas as infomações dos usuarios de uma só vez!
+foreach($funcionarios as $f) {
+    echo "Nome do colaborador:". $f["nome"], "<br>";
+    echo "Cargo do colaborador:". $f["cargo"], "<br>";
+    echo "Setor do colaborador:". $f["setor"], "<br>";
+    echo "Idade do colaborador:". $f["idade"], "<br>";
+    echo "<hr>";
+};
+?>
